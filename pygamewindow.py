b6357d69f90c0544dea23ac322a4e94c4bd51b0d
Jayden-101 <jaydenmaxmillian2@gmail.com>
2025-08-19 17:58:33 +1200

    Pygame Window

---
+++ pygamewindow.py
@@ -0,0 +1,11 @@
+# Import tkinter
+from tkinter import *
+
+# Create window
+window = Tk()
+
+# set window title and geometry
+window.title('Demo window')
+window.geometry('400x300')
+
+window.mainloop()
